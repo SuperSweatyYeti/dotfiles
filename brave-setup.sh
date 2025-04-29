934bdddb0d4424ac174a93a5b59bb2c61b97ec3a
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-04-29 16:28:43 -0700

    update brave service file

--- brave-setup.sh
+++ brave-setup.sh
@@ -8,7 +8,13 @@ VERTICAL_TABS=$(jq '.brave.tabs.vertical_tabs_enabled' "$PREFERENCES_FILE" )
 if [ "$VERTICAL_TABS" == "false" ]; then
   jq '.brave.tabs.vertical_tabs_enabled = true' "$PREFERENCES_FILE" > "$PREFERENCES_FILE.tmp" && mv "$PREFERENCES_FILE.tmp" "$PREFERENCES_FILE"
   echo "Updated vertical_tabs_enabled to true."
-else
+  exit 0
+elif [ "$VERTICAL_TABS" == "true" ]; then
   echo "vertical_tabs_enabled is already true."
+  exit 0
+else
+  echo "[ERROR] The 'vertical_tabs_enabled' option could not be found in the Brave Browser Preferences file at:"
+  echo "$PREFERENCES_FILE"
+  exit 1
 fi
 
