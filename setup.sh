039a2fe25161d58865fcfad8c9d69a61ea597e59
Sweaty Yeti <supersweatyyeti@gmail.com>
2026-03-14 22:22:45 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Setup tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+# Setup keyd config
+echo "Setting up keyd config file..."
+sudo mkdir -p /etc/keyd
+sudo cp ~/dotfiles/keyd.conf /etc/keyd/default.conf
 
+# Setup tmux plugin manager
+echo "Git cloning tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
