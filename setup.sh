16f18f3abfa79aee7d99b3882bb4283a50683970
Sweaty Yeti <supersweatyyeti@gmail.com>
2026-03-14 22:37:26 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
+# Pre-create all required parent directories in $HOME
+# This scans the dotfiles repo for directories and mirrors them in ~
+# so that `stow --adopt` doesn't fail on missing paths.
+cd ~/dotfiles || exit
+find . -type d \
+  ! -path './.git' \
+  ! -path './.git/*' \
+  -exec mkdir -p "$HOME/{}" \;
+
+# Init and download submodules
+git submodule update --init --recursive
+
+# Checkout master branches for submodules
+cd ~/dotfiles/.config/nvim || exit
+git checkout master
+
+cd ~/dotfiles/.config/tmux/plugins/tpm || exit
+git checkout master
+
+# Setup tmux plugin manager (skip if already cloned via submodule)
+if [ ! -d ~/.config/tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+fi
+
+# Symlink entire directories so new files are immediately tracked by git
+# -s = symbolic link, -f = force overwrite, -n = don't follow existing dir
+ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/.config/putty ~/.config/putty
+ln -sfn ~/dotfiles/.config/rmpc ~/.config/rmpc
+ln -sfn ~/dotfiles/.config/yazi ~/.config/yazi
+ln -sfn ~/dotfiles/.config/rio ~/.config/rio
+ln -sfn ~/dotfiles/.config/tmux ~/.config/tmux
+
 # Setup keyd config
-echo "Setting up keyd config file..."
 sudo mkdir -p /etc/keyd
-sudo cp ~/dotfiles/keyd.conf /etc/keyd/default.conf
+sudo ln -sf ~/dotfiles/keyd.conf /etc/keyd/default.conf
 
-# Setup tmux plugin manager
-echo "Git cloning tmux plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-
+# Now stow is safe to run
+cd ~/dotfiles || exit
+stow . --adopt
